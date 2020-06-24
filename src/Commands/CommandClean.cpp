a061ca5621510e3d1fa79a5debef778fcb194937
Eric Beuque <eric.beuque@jet1oeil.com>
2020-06-24 15:09:02 +0000

    Add clean commands

--- src/Commands/CommandClean.cpp
+++ src/Commands/CommandClean.cpp
@@ -3,3 +3,56 @@
 //
 
 #include "CommandClean.h"
+
+CommandClean::CommandClean() : AbstractPackageCommand("clean")
+{
+
+}
+
+CommandClean::~CommandClean()
+{
+
+}
+
+bool CommandClean::doExecute()
+{
+    bool bRes;
+
+    // Execute steps
+    bRes = doExecuteStep("clean", getRootPackageDir());
+
+    if(bRes){
+        bRes = removeDir(getRootPackageDir().absolutePath());
+        if(!bRes){
+            qCritical("[clean] Error while cleaning directory");
+        }
+    }
+
+    return bRes;
+}
+
+bool CommandClean::removeDir(const QString& szDirName)
+{
+    bool bResult = true;
+
+    QDir dir(szDirName);
+    if (dir.exists(szDirName)){
+        QFileInfoList fileInfoList = dir.entryInfoList(QDir::NoDotAndDotDot | QDir::System | QDir::Hidden  | QDir::AllDirs | QDir::Files, QDir::DirsFirst);
+        QFileInfoList::const_iterator iter;
+        for(iter = fileInfoList.constBegin(); iter != fileInfoList.constEnd(); ++iter){
+            const QFileInfo& fileInfo = (*iter);
+            if (fileInfo.isDir()) {
+                bResult = removeDir(fileInfo.absoluteFilePath());
+            }else{
+                bResult = QFile::remove(fileInfo.absoluteFilePath());
+            }
+
+            if (!bResult) {
+                return bResult;
+            }
+        }
+        qDebug("[clean] Cleaning directory %s", qPrintable(dir.path()));
+        bResult = dir.rmdir(szDirName);
+    }
+    return bResult;
+}
--- src/Commands/CommandClean.h
+++ src/Commands/CommandClean.h
@@ -5,9 +5,18 @@
 #ifndef STARDEPS_COMMANDCLEAN_H
 #define STARDEPS_COMMANDCLEAN_H
 
+#include "AbstractPackageCommand.h"
 
-class CommandClean {
+class CommandClean : public AbstractPackageCommand {
+public:
+    CommandClean();
+    virtual ~CommandClean();
 
+protected:
+    bool doExecute();
+
+private:
+    bool removeDir(const QString& szDirName);
 };
 
 
