0296725bb122bd285f082ca3cc1af7760deca4a7
Eric Beuque <eric.beuque@jet1oeil.com>
2020-07-02 17:27:29 +0000

    Add SVN connector

--- src/Connector/ConnectorSVN.cpp
+++ src/Connector/ConnectorSVN.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "ConnectorSVN.h"
+
+ConnectorSVN::ConnectorSVN(const Environment& env) : AbstractConnector(env)
+{
+
+}
+
+ConnectorSVN::~ConnectorSVN()
+{
+
+}
+
+bool ConnectorSVN::svn_checkout(const QString& szURL, const QDir& dirWorkingDirectory)
+{
+    QStringList listArgs;
+    qDebug("[svn] Checkout sources to version %s", qPrintable(szURL));
+    listArgs.append("checkout");
+    listArgs.append(szURL);
+    listArgs.append(dirWorkingDirectory.path());
+    return m_shell.runCommand(m_env.getSVNExe(), listArgs);
+}
+
+bool ConnectorSVN::svn_update(const QDir& dirWorkingDirectory)
+{
+    QStringList listArgs;
+    qDebug("[svn] Updating sources");
+    listArgs.append("update");
+    return m_shell.runCommand(m_env.getSVNExe(), listArgs, dirWorkingDirectory);
+}
--- src/Connector/ConnectorSVN.h
+++ src/Connector/ConnectorSVN.h
@@ -5,12 +5,17 @@
 #ifndef STARDEPS_CONNECTORSVN_H
 #define STARDEPS_CONNECTORSVN_H
 
+#include "AbstractConnector.h"
 
+class ConnectorSVN : public AbstractConnector
+{
+public:
+    ConnectorSVN(const Environment& env);
+    virtual ~ConnectorSVN();
 
-class ConnectorSVN {
-
+    bool svn_checkout(const QString& szURL, const QDir& dirWorkingDirectory);
+    bool svn_update(const QDir& dirWorkingDirectory);
 };
-
 
 
 #endif //STARDEPS_CONNECTORSVN_H
